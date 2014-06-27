918cff1d9cf654926078caf88f5bfc8b6dc04784
Alan Gardner <alan@armoin.com>
2014-06-27 17:33:47 +0100

    Fleshing out test for My Assets page
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/features/my_assets_spec.rb
+++ spec/features/my_assets_spec.rb
@@ -2,14 +2,39 @@ require 'rails_helper'
 
 feature 'As a user I want to be able to manage my assets' do
   feature 'So that I can view my existing assets' do
-    scenario 'displays title' do
-      visit '/assets'
-      expect(page).to have_text('My Assets')
+    context 'an asset' do
+      let(:asset) { find('.asset:first') }
+
+      before :each do
+        visit '/assets'
+      end
+
+      it 'has a title' do
+        expect(asset.find('.title')).to have_text("Arthur's Seat")
+      end
+
+      it 'has an image' do
+        img = asset.find('img')
+        expect(img['src']).to have_content("meadows.jpg")
+        expect(img['alt']).to have_content("Arthur's Seat from the Meadows")
+      end
+
+      it 'has a link to more details' do
+        expect(asset).to have_link('View details')
+      end
     end
 
     scenario 'displays all existing assets' do
       visit '/assets'
       expect(page).to have_css('.asset', count: 5)
+    end
+  end
+
+  feature 'So that I can add new assets' do
+    scenario 'allows the user to add a new asset' do
+      visit '/assets'
+      click_link('add-asset')
+      expect(current_path).to eql('/assets/new')
     end
   end
 end
