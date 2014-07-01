bb247422270ed25d58b67a525f97edc8d378b80c
Alan Gardner <alan@armoin.com>
2014-07-01 08:27:33 +0100

    Helps if I actually call the CodeClimate test reporter
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,10 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'webmock/rspec'
 
 RSpec.configure do |config|
