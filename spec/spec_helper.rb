a7d3e4152c37ff250391a4ff755fe81dfe78457b
Alan Gardner <alan@armoin.com>
2014-07-01 08:54:37 +0100

    Ignoring CodeClimate in webmock
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 require 'webmock/rspec'
+WebMock.disable_net_connect!(:allow => "codeclimate.com")
 
 RSpec.configure do |config|
   config.before(:each) do
