65f83edf61f3538169682146c8c4b3bfe4aad258
Alan Gardner <alan@armoin.com>
2014-07-07 16:48:54 +0100

    Test coverage
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/features/add_asset_spec.rb
+++ spec/features/add_asset_spec.rb
@@ -32,9 +32,15 @@ feature 'adding new assets' do
 
   scenario 'adding a new asset adds it to the users assets' do
     visit '/assets/new'
+
+    select 'image', from: 'asset[file_type]'
     attach_file :file, File.join(File.dirname(__FILE__), '../fixtures/files/test.jpg')
     fill_in 'asset[title]', with: 'A Test'
-    click_button 'Add Asset'
+    fill_in 'asset[date]', with: '2014-07-07'
+    fill_in 'asset[description]', with: 'Oh the heady days of testing.'
+
+    click_button 'Save'
+
     # puts page.html
     expect(current_path).to eq('/assets')
     expect(page).to have_selector('.title', text: 'A Test')
