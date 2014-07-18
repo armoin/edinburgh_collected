182530dac84bc3d57548283fa763ddcf826925bd
Alan Gardner <alan@armoin.com>
2014-07-18 11:26:37 +0100

    Tweaks
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( everything_else.css jasmine.css spec.css jasminerice.js spec.js )
+Rails.application.config.assets.precompile += %w( temporary.css jasmine.css spec.css jasminerice.js spec.js )
