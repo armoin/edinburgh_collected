a75bf8045392797834821aaada77e65c61c49ede
Alan Gardner <alan@armoin.com>
2014-07-31 03:07:00 +0100

    Adding authentication controls to layout
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :logged_in?
+
+  def logged_in?
+    session[:auth_token].present?
+  end
 end
