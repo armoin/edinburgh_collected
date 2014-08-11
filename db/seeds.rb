ea4f49837ddbebcbe3014543aafd82a8edde7f28
Alan Gardner <alan@armoin.com>
2014-08-11 12:19:31 +0100

    Adding areas by seeding
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+city    = 'Edinburgh'
+country = 'United Kingdom'
+areas   = [
+  "Balerno",
+  "Blackhall",
+  "Bruntsfield",
+  "Clermiston",
+  "Colinton",
+  "Corstorphine",
+  "Craigmillar",
+  "Cramond",
+  "Currie",
+  "Dalry",
+  "Dean Village",
+  "Drumbrae",
+  "Duddingston",
+  "Fountainbridge",
+  "Gilmerton",
+  "Gorgie",
+  "Granton",
+  "Joppa",
+  "Kirkliston",
+  "Leith",
+  "Liberton",
+  "Morningside",
+  "Muirhouse",
+  "New Town",
+  "Newhaven",
+  "Newington",
+  "Old Town",
+  "Oxgangs",
+  "Piershill",
+  "Pilton",
+  "Portobello",
+  "Ratho",
+  "Sighthill",
+  "South Queensferry",
+  "Stockbridge",
+  "Wester Hailes",
+]
+
+areas.each do |area|
+  p area: area
+  Area.create_with(city: city, country: country).find_or_create_by(name: area)
+end
