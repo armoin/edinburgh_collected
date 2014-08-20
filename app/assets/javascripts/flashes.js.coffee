450f5106b5ff74f7475519037a04daf51a9e81ac
Alan Gardner <alan@armoin.com>
2014-08-20 15:53:12 +0100

    Adding a living styleguide to the app
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/assets/javascripts/flashes.js.coffee
+++ app/assets/javascripts/flashes.js.coffee
@@ -1,5 +1,5 @@
 class @FlashManager
-  @closeFlashes = -> $('.flashes').fadeOut(2000)
+  @closeFlashes = -> $('body:not(.styleguide) .flashes').fadeOut(2000)
 
 $(document).ready ->
   setTimeout FlashManager.closeFlashes, 2000
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # styleguide
+  get 'styleguide' => 'styleguide#blocks'
+  get 'styleguide/blocks' => 'styleguide#blocks'
+  get 'styleguide/elements' => 'styleguide#elements'
+
   namespace :my do
     resources :memories
   end
