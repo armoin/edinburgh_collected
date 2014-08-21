8c7156846a5e9352bb2633901dbaccbd103d931e
Alan Gardner <alan@armoin.com>
2014-08-21 14:19:55 +0100

    Missed one
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/fabricators/memory_fabricator.rb
+++ spec/fabricators/memory_fabricator.rb
@@ -1,17 +1,3 @@
-# Fabricator(:memory, from: :photo) do
-#   user
-#   area
-#   year        "2014"
-#   month       "5"
-#   day         "4"
-#   title       "A test"
-#   description "This is a test."
-#   attribution "Bobby Tables"
-#   location    "Kings Road"
-#   source      Rack::Test::UploadedFile.new(File.join(File.join(Rails.root, 'spec', 'fixtures', 'files'), 'test.jpg'))
-#   categories(rand: 3) { |attrs, i| Fabricate(:category) }
-# end
-
 Fabricator(:photo_memory, from: :photo) do
   user
   area
