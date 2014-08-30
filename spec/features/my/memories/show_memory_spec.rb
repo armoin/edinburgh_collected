46a2ee743c2bbd797671fc5a487284032349f361
Alan Gardner <alan@armoin.com>
2014-08-30 13:07:14 +0100

    Feature spec fixen
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/features/my/memories/show_memory_spec.rb
+++ spec/features/my/memories/show_memory_spec.rb
@@ -29,48 +29,7 @@ feature 'As a user I want to be able to view one of my memories', slow: true do
       end
 
       context 'and memory is mine' do
-        let(:memory_class) { find('.memory') }
-
-        it 'has a title' do
-          expect(memory_class.find('.title')).to have_text("A test")
-        end
-
-        it 'has an image' do
-          img = memory_class.find('img')
-          expect(img['src']).to have_content("test.jpg")
-          expect(img['alt']).to have_content("A test")
-        end
-
-        it 'has a type' do
-          expect(memory_class.find('.type')).to have_text("Photo")
-        end
-
-        it 'has a description' do
-          expect(memory_class.find('.description')).to have_text("This is a test.")
-        end
-
-        it "has a date" do
-          date_text = memory_class.find('.date').native.text
-          expect(date_text).to eql("\nDates from:\n4th May 2014\n")
-        end
-
-        it 'has an attribution' do
-          expect(memory_class.find('.attribution')).to have_text("Bobby Tables")
-        end
-
-        it 'has an address' do
-          expect(memory_class.find('.address')).to have_text("Kings Road, Portobello")
-        end
-
-        # lat and long come from spec/support/geocoder.rb default stub
-        it 'has a longitude' do
-          expect(memory_class.find('.longitude')).to have_text("55.9578751")
-        end
-
-        # lat and long come from spec/support/geocoder.rb default stub
-        it 'has a latitude' do
-          expect(memory_class.find('.latitude')).to have_text("-3.1196158")
-        end
+        it_behaves_like 'a memory preview'
       end
 
       scenario 'lets me go back to the previous page' do
