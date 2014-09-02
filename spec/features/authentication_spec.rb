f5cb4abe723b5a4ae4a2e0dd6d194881b600e8f3
Alan Gardner <alan@armoin.com>
2014-09-02 16:25:54 +0100

    Converting sign up to simpleform
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -4,11 +4,11 @@ feature 'As a user who wants to add content to the site' do
   scenario 'I can sign up for an account without a screen name' do
     visit '/signup'
 
-    fill_in 'First name',            with: 'Bobby'
-    fill_in 'Last name',             with: 'Tables'
-    fill_in 'Email',                 with: 'bobby@example.com'
-    fill_in 'Password',              with: 's3cr3t'
-    fill_in 'Password confirmation', with: 's3cr3t'
+    fill_in 'First name',                    with: 'Bobby'
+    fill_in 'Last name',                     with: 'Tables'
+    fill_in 'Email',                         with: 'bobby@example.com'
+    fill_in 'user_password',                 with: 's3cr3t'
+    fill_in 'user_password_confirmation',    with: 's3cr3t'
 
     click_button 'Sign Up'
 
@@ -23,12 +23,12 @@ feature 'As a user who wants to add content to the site' do
   scenario 'I can sign up for an account with a screen name' do
     visit '/signup'
 
-    fill_in 'First name',               with: 'Bobby'
-    fill_in 'Last name',                with: 'Tables'
-    fill_in 'What should we call you?', with: 'Bob'
-    fill_in 'Email',                    with: 'bobby@example.com'
-    fill_in 'Password',                 with: 's3cr3t'
-    fill_in 'Password confirmation',    with: 's3cr3t'
+    fill_in 'First name',                    with: 'Bobby'
+    fill_in 'Last name',                     with: 'Tables'
+    fill_in 'What should we call you?',      with: 'Bob'
+    fill_in 'Email',                         with: 'bobby@example.com'
+    fill_in 'user_password',                 with: 's3cr3t'
+    fill_in 'user_password_confirmation',    with: 's3cr3t'
 
     click_button 'Sign Up'
 
@@ -44,12 +44,12 @@ feature 'As a user who wants to add content to the site' do
   scenario "I can sign in to my account" do
     visit '/signup'
 
-    fill_in 'First name',               with: 'Bobby'
-    fill_in 'Last name',                with: 'Tables'
-    fill_in 'Email',                    with: 'bobby@example.com'
-    fill_in 'What should we call you?', with: 'Bob'
-    fill_in 'Password',                 with: 's3cr3t'
-    fill_in 'Password confirmation',    with: 's3cr3t'
+    fill_in 'First name',                    with: 'Bobby'
+    fill_in 'Last name',                     with: 'Tables'
+    fill_in 'What should we call you?',      with: 'Bob'
+    fill_in 'Email',                         with: 'bobby@example.com'
+    fill_in 'user_password',                 with: 's3cr3t'
+    fill_in 'user_password_confirmation',    with: 's3cr3t'
 
     click_button 'Sign Up'
 
