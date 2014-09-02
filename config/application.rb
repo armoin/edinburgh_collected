2102d08646049f06ae0c4d924c0a72c006bb161f
Alan Gardner <alan@armoin.com>
2014-09-03 00:52:54 +0100

    Adding host
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module EdinburghStories
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_mailer.default_url_options = {:host => ENV['HOST']}
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
