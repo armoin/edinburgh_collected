79180fcb373998457d59f9dcf57c5dce29a09f21
jenthomson <jen@upcloseandpixelated.com>
2014-09-23 17:28:33 +0100

    backgrounds and seach bar styles
    Signed-off-by: jenthomson <jen@upcloseandpixelated.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141106105359) do
+=======
+ActiveRecord::Schema.define(version: 20141030141417) do
+>>>>>>> backgrounds and seach bar styles
 
   create_table "areas", force: true do |t|
     t.string   "name",       null: false
