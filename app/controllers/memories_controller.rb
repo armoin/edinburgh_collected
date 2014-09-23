93e1828c88b8452121cc1091d6b0a87bb58a26ea
Alan Gardner <alan@armoin.com>
2014-09-23 10:24:59 +0100

    Basic API around memories
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,9 +1,13 @@
 class MemoriesController < ApplicationController
+  respond_to :html, :json
+
   def index
     @memories = Memory.all
+    respond_with(@memories)
   end
 
   def show
     @memory = Memory.find(params[:id])
+    respond_with(@memory)
   end
 end
--- spec/controllers/memories_controller_spec.rb
+++ spec/controllers/memories_controller_spec.rb
@@ -2,35 +2,52 @@ require 'rails_helper'
 
 describe MemoriesController do
   describe 'GET index' do
-    let(:expected) { [Memory.new, Memory.new] }
+    let(:memories) { [Memory.new, Memory.new] }
 
     before(:each) do
-      allow(Memory).to receive(:all).and_return(expected)
-    end
-
-    it "is successful" do
-      get :index
-      expect(response).to be_success
-      expect(response.status).to eql(200)
-    end
-
-    it "renders the index page" do
-      get :index
-      expect(response).to render_template(:index)
+      allow(Memory).to receive(:all).and_return(memories)
     end
 
     it "fetches the memories and assigns them" do
       get :index
-      expect(assigns(:memories)).to eql(expected)
+      expect(assigns(:memories)).to eql(memories)
+    end
+
+    context 'when request is for HTML' do
+      it "is successful" do
+        get :index
+        expect(response).to be_success
+        expect(response.status).to eql(200)
+      end
+
+      it "renders the index page" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+    context 'when request is for JSON' do
+      it 'is successful' do
+        get :index, format: :json
+        expect(response).to be_success
+        expect(response.status).to eql(200)
+      end
+
+      it "provides the expected JSON" do
+        expected_json = memories.to_json
+        get :index, format: :json
+        expect(response.body).to eql(expected_json)
+      end
     end
   end
 
   describe 'GET show' do
     let(:memory) { Memory.new }
+    let(:format) { :html }
 
     before :each do
       allow(Memory).to receive(:find) { memory }
-      get :show, id: '123'
+      get :show, id: '123', format: format
     end
 
     it "is successful" do
@@ -47,16 +64,45 @@ describe MemoriesController do
         expect(assigns(:memory)).to eql(memory)
       end
 
-      it "renders the show page" do
-        expect(response).to render_template(:show)
+      context "when requesting HTML" do
+        it "renders the show page" do
+          expect(response).to render_template(:show)
+        end
+      end
+
+      context "when requesting JSON" do
+        let(:format) { :json }
+
+        it "provides the expected JSON" do
+          expect(response.body).to eql(memory.to_json)
+        end
       end
     end
 
     context "fetch is not successful" do
-      it "renders the not found page" do
+      before :each do
         allow(Memory).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
-        get :show, id: '123'
-        expect(response).to render_template('exceptions/not_found')
+      end
+
+      context "when requesting HTML" do
+        before :each do
+          get :show, id: '123'
+        end
+
+        it "renders the not found page" do
+          expect(response).to render_template('exceptions/not_found')
+        end
+
+        it "returns an error status" do
+          expect(response.status).to eq(404)
+        end
+      end
+
+      context "when requesting JSON" do
+        it "returns an error status" do
+          get :show, id: '123', format: :json
+          expect(response.status).to eq(404)
+        end
       end
     end
   end
