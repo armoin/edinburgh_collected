7418e1c39d31e0f259a3a0cd9f229f340129f878
jenthomson <jen@upcloseandpixelated.com>
2014-10-03 12:26:01 +0100

    work on masonry
    Signed-off-by: jenthomson <jen@upcloseandpixelated.com>

--- vendor/assets/javascripts/masonry-activate.js
+++ vendor/assets/javascripts/masonry-activate.js
@@ -4,53 +4,52 @@ jQuery(window).load(function () {
 
 
 
-	// Takes the gutter width from the bottom margin of .post
+	// Takes the gutter width from the bottom margin of .memory
 
-	var gutter = parseInt(jQuery('.post').css('marginBottom'));
-	var container = jQuery('#posts');
+	var gutter = parseInt(jQuery('.memory').css('marginBottom'));
+	var container = jQuery('#memories');
 
 
 
-	// Creates an instance of Masonry on #posts
+	// Creates an instance of Masonry on #memories
 
 	container.masonry({
 		gutter: gutter,
-		itemSelector: '.post',
-		columnWidth: '.post'
+		itemSelector: '.memory',
+		columnWidth: '.memory'
 	});
 
 
 
-	// This code fires every time a user resizes the screen and only affects .post elements
+	// This code fires every time a user resizes the screen and only affects .memory elements
 	// whose parent class isn't .container. Triggers resize first so nothing looks weird.
 
 	jQuery(window).bind('resize', function () {
-		if (!jQuery('#posts').parent().hasClass('container')) {
+		if (!jQuery('#memories').parent().hasClass('container')) {
 
 
 
 			// Resets all widths to 'auto' to sterilize calculations
 
-			post_width = jQuery('.post').width() + gutter;
-			jQuery('#posts, body > #grid').css('width', 'auto');
+			memory_width = jQuery('.memory').width() + gutter;
+			jQuery('#memories, body > #grid').css('width', 'auto');
 
 
 
-			// Calculates how many .post elements will actually fit per row. Could this code be cleaner?
+			// Calculates how many .memory elements will actually fit per row. Could this code be cleaner?
 
-			posts_per_row = jQuery('#posts').innerWidth() / post_width;
-			floor_posts_width = (Math.floor(posts_per_row) * post_width) - gutter;
-			ceil_posts_width = (Math.ceil(posts_per_row) * post_width) - gutter;
-			posts_width = (ceil_posts_width > jQuery('#posts').innerWidth()) ? floor_posts_width : ceil_posts_width;
-			if (posts_width == jQuery('.post').width()) {
-				posts_width = '100%';
+			memories_per_row = jQuery('#memories').innerWidth() / memory_width;
+			floor_memories_width = (Math.floor(memories_per_row) * memory_width) - gutter;
+			ceil_memories_width = (Math.ceil(memories_per_row) * memory_width) - gutter;
+			memories_width = (ceil_memories_width > jQuery('#memories').innerWidth()) ? floor_memories_width : ceil_memories_width;
+			if (memories_width == jQuery('.memory').width()) {
+				memories_width = '100%';
 			}
 
 
 
 			// Ensures that all top-level elements have equal width and stay centered
 
-			jQuery('#posts, #grid').css('width', posts_width);
 			jQuery('#grid').css({'margin': '0 auto'});
 
 
