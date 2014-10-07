70bba206811b27f72b67b908b21afd33195c8968
Alan Gardner <alan@armoin.com>
2014-10-07 17:30:28 +0100

    WIP: starting to swap feature specs for view specs
    Signed-off-by: Alan Gardner <alan@armoin.com>

---
+++ spec/views/my/memories/show.html.erb_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe "my/memories/show.html.erb" do
+  let(:memory) { Fabricate.build(:photo_memory, id: 123) }
+
+  it "has a Back link to the my_memories page" do
+    assign(:memory, memory)
+    render
+    expect(rendered).to have_link('Back', href: my_memories_path)
+  end
+
+  context "when memory doesn't belong to the user" do
+    before :each do
+      assign(:memory, memory)
+      allow(view).to receive(:belongs_to_user?).and_return(false)
+      render
+    end
+
+    it "does not have an edit link" do
+      expect(rendered).not_to have_link('Edit', href: edit_my_memory_path(memory))
+    end
+
+    it "does not have a delete link" do
+      expect(rendered).not_to have_link('Delete', href: my_memory_path(memory))
+    end
+  end
+
+  context "when memory belongs to the user" do
+    before :each do
+      assign(:memory, memory)
+      allow(view).to receive(:belongs_to_user?).and_return(true)
+      render
+    end
+
+    it "has an edit link" do
+      expect(rendered).to have_link('Edit', href: edit_my_memory_path(memory))
+    end
+
+    it "has a delete link" do
+      expect(rendered).to have_link('Delete', href: my_memory_path(memory))
+    end
+  end
+
+  it_behaves_like "a memory show page"
+end
