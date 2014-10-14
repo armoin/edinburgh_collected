79c973ef91dd2322fe2917c99f323ee3074be0a6
Alan Gardner <alan@armoin.com>
2014-10-14 16:20:21 +0100

    Users can remove memories from their scrapbooks
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/models/scrapbook.rb
+++ app/models/scrapbook.rb
@@ -1,6 +1,6 @@
 class Scrapbook < ActiveRecord::Base
   belongs_to :user
-  has_many :scrapbook_memories
+  has_many :scrapbook_memories, dependent: :destroy
   has_many :memories, through: :scrapbook_memories
 
   validates :title, :user, presence: true
---
+++ spec/views/my/scrapbooks/edit.html.erb_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+describe 'my/scrapbooks/edit.html.erb' do
+  let(:scrapbook)           { Fabricate.build(:scrapbook, id: 132) }
+  let(:memories)            { stub_memories(3) }
+  let(:scrapbook_memories)  { memories.map.with_index{|m,i| double(id: i, scrapbook: scrapbook, memory: m)} }
+
+  before :each do
+    allow(scrapbook).to receive(:scrapbook_memories).and_return(scrapbook_memories)
+    assign(:scrapbook, scrapbook)
+    render
+  end
+
+  it "has an 'All my scrapbooks' link to the my_scrapbooks page" do
+    render
+    expect(rendered).to have_link('All my scrapbooks', href: my_scrapbooks_path)
+  end
+
+  context "when memory doesn't belong to the user" do
+    before :each do
+      allow(view).to receive(:belongs_to_user?).and_return(false)
+      render
+    end
+
+    it "does not have an 'Add more memories' link" do
+      expect(rendered).not_to have_link('Add more memories', href: memories_path)
+    end
+
+    it "does not have a show link" do
+      expect(rendered).not_to have_link('Show', href: my_scrapbook_path(scrapbook))
+    end
+
+    it "does not have a delete link" do
+      expect(rendered).not_to have_link('Delete', href: my_scrapbook_path(scrapbook))
+    end
+  end
+
+  context "when memory belongs to the user" do
+    before :each do
+      allow(view).to receive(:belongs_to_user?).and_return(true)
+      render
+    end
+
+    it "has an 'Add more memories' link" do
+      expect(rendered).to have_link('Add more memories', href: memories_path)
+    end
+
+    it "has a show link" do
+      expect(rendered).to have_link('Show', href: my_scrapbook_path(scrapbook))
+    end
+
+    it "has a delete link" do
+      expect(rendered).to have_link('Delete', href: my_scrapbook_path(scrapbook))
+    end
+  end
+
+  describe 'editing the scrapbook details' do
+    it 'lets the user edit the title' do
+      expect(rendered).to have_css('input#scrapbook_title', count: 1)
+    end
+
+    it 'lets the user edit the description' do
+      expect(rendered).to have_css('textarea#scrapbook_description', count: 1)
+    end
+  end
+
+  describe "the scrapbook's memories" do
+    it "displays all of the scrapbook's memories" do
+      expect(rendered).to have_css('.memory', count: 3)
+    end
+
+    it "lets the user delete a memory" do
+      expect(rendered).to have_css(".memory a[data-method='delete']", text: 'Delete', count: 3)
+    end
+  end
+end
