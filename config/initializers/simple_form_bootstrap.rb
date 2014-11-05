53f273db88e47af595f57c856d80eb6c9d122014
jenthomson <jen@upcloseandpixelated.com>
2014-11-05 11:08:11 +0000

    Move Asterisk on forms
    Signed-off-by: jenthomson <jen@upcloseandpixelated.com>

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -70,4 +70,6 @@ SimpleForm.setup do |config|
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
   config.default_wrapper = :bootstrap
+  config.label_text = lambda { |label, required| "#{label} #{required}" }
+
 end
