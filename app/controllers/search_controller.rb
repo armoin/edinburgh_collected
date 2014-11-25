561036a48f5672d348b08ceddc4716a3c8f33d61
Alan Gardner <alan@armoin.com>
2014-11-25 07:21:34 +0000

    Fixing broken pagination
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @memories = Memory.text_search(params[:query]).page(params[:current_page]).per(30)
+    @memories = Memory.text_search(params[:query]).page(params[:page]).per(30)
   end
 end
 
