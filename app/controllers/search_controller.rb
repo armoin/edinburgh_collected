d4bcd256abf48f4914b79d371326b00ff7500ab5
Alan Gardner <alan@armoin.com>
2014-11-27 17:02:09 +0000

    Redirect to browse if search query is blank
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
+    redirect_to memories_path if params[:query].blank?
     @memories = Memory.approved.text_search(params[:query]).page(params[:page]).per(30)
   end
 end
--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe SearchController do
   let(:approved_memories) { double('approved_memories') }
   let(:returned_memories) { double('returned_memories') }
-  let(:query)             { "test search" }
   let(:format)            { :html }
 
   before(:each) do
@@ -18,33 +17,53 @@ describe SearchController do
       get :index, format: format, query: query
     end
 
-    it "fetches the approved memories" do
-      expect(Memory).to have_received(:approved)
+    context 'when no query is given' do
+      let(:query) { nil }
+
+      it "redirects to the browse memories page" do
+        expect(response).to redirect_to(memories_path)
+      end
     end
 
-    it "searches for the given query" do
-      expect(approved_memories).to have_received(:text_search).with(query)
+    context 'when a blank query is given' do
+      let(:query) { "" }
+
+      it "redirects to the browse memories page" do
+        expect(response).to redirect_to(memories_path)
+      end
     end
 
-    it "paginates the results 30 to a page" do
-      expect(returned_memories).to have_received(:page)
-      expect(returned_memories).to have_received(:per).with(30)
-    end
+    context 'when a query is given' do
+      let(:query) { "test search" }
 
-    it "assigns the returned memories" do
-      expect(assigns(:memories)).to eql(returned_memories)
-    end
-
-    context 'when request is for HTML' do
-      let(:format) { :html }
-
-      it "is successful" do
-        expect(response).to be_success
-        expect(response.status).to eql(200)
+      it "fetches the approved memories" do
+        expect(Memory).to have_received(:approved)
       end
 
-      it "renders the index page" do
-        expect(response).to render_template(:index)
+      it "searches for the given query" do
+        expect(approved_memories).to have_received(:text_search).with(query)
+      end
+
+      it "paginates the results 30 to a page" do
+        expect(returned_memories).to have_received(:page)
+        expect(returned_memories).to have_received(:per).with(30)
+      end
+
+      it "assigns the returned memories" do
+        expect(assigns(:memories)).to eql(returned_memories)
+      end
+
+      context 'when request is for HTML' do
+        let(:format) { :html }
+
+        it "is successful" do
+          expect(response).to be_success
+          expect(response.status).to eql(200)
+        end
+
+        it "renders the index page" do
+          expect(response).to render_template(:index)
+        end
       end
     end
   end
