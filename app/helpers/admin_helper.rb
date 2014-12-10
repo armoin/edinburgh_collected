66fe6e0139f87429d7d26d2bc3d4019d0b724341
Alan Gardner <alan@armoin.com>
2014-12-10 17:17:40 +0000

    Fixing missing moderation toolbar
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,6 +1,6 @@
 module AdminHelper
   def show_moderation_panel?
-    current_memory_index_path.match /\/moderation\//
+    current_memory_index_path.match /moderated/
   end
 end
 
--- spec/helpers/admin_helper_spec.rb
+++ spec/helpers/admin_helper_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 describe AdminHelper do
   describe '#show_moderation_panel?' do
-    it 'is true if the current controller is a moderation one' do
-      allow(helper).to receive(:current_memory_index_path).and_return('/admin/moderation/memories')
+    it 'is true if the current index path is admin/moderated' do
+      allow(helper).to receive(:current_memory_index_path).and_return('/admin/moderated')
+      expect(helper.show_moderation_panel?).to be_truthy
+    end
+
+    it 'is true if the current index path is admin/unmoderated' do
+      allow(helper).to receive(:current_memory_index_path).and_return('/admin/unmoderated')
       expect(helper.show_moderation_panel?).to be_truthy
     end
 
