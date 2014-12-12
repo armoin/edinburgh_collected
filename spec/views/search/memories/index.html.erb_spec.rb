22940a210650900b69b3f5499db9232e98d7af4c
Alan Gardner <alan@armoin.com>
2014-12-12 09:54:39 +0000

    Adding test to the no results page
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/views/search/memories/index.html.erb_spec.rb
+++ spec/views/search/memories/index.html.erb_spec.rb
@@ -2,34 +2,65 @@ require 'rails_helper'
 
 describe 'search/memories/index.html.erb' do
   let(:user)           { Fabricate(:user) }
-  let(:memories)       { Fabricate.times(3, :photo_memory, user: user) }
-  let(:paged_memories) { Kaminari.paginate_array(memories).page(1) }
   let(:params_stub)    { {query: 'test search'}  }
 
-  before :each do
-    allow(view).to receive(:current_user).and_return(user)
-    allow(view).to receive(:params).and_return(params_stub)
-    assign(:memories, paged_memories)
-    render
-  end
+  context 'when there are no results' do
+    let(:memories)       { [] }
+    let(:paged_memories) { Kaminari.paginate_array(memories).page(1) }
 
-  it 'displays the result count' do
-    expect(rendered).to have_css('#contentHeader', text: "Found 3 matches for \"#{params_stub[:query]}\"")
-  end
+    before :each do
+      allow(view).to receive(:current_user).and_return(user)
+      allow(view).to receive(:params).and_return(params_stub)
+      assign(:memories, paged_memories)
+      render
+    end
 
-  it "doesn't show an add button" do
-    expect(rendered).not_to have_css('.memory.add')
-  end
+    it 'displays the result count' do
+      expect(rendered).to have_css('#contentHeader', text: "Found 0 matches for \"#{params_stub[:query]}\"")
+    end
 
-  context 'a memory' do
-    let(:memory) { memories.first }
+    it "doesn't show an add button" do
+      expect(rendered).not_to have_css('.memory.add')
+    end
 
-    it 'is a link to the show page for that memory' do
-      expect(rendered).to have_css("a.memory[href=\"#{memory_path(memory)}\"]")
+    it "does not display any results" do
+      expect(rendered).not_to have_css('.memory')
+    end
+
+    it "displays a no results message" do
+      expect(rendered).to have_css('p.no-results', text: "Sorry, but we couldn't find any results for \"#{params_stub[:query]}\"")
     end
   end
 
-  it_behaves_like 'a memory index'
-  it_behaves_like 'paginated content'
+  context 'when there are results' do
+    let(:memories)       { Fabricate.times(3, :photo_memory, user: user) }
+    let(:paged_memories) { Kaminari.paginate_array(memories).page(1) }
+
+    before :each do
+      allow(view).to receive(:current_user).and_return(user)
+      allow(view).to receive(:params).and_return(params_stub)
+      assign(:memories, paged_memories)
+      render
+    end
+
+    it 'displays the result count' do
+      expect(rendered).to have_css('#contentHeader', text: "Found 3 matches for \"#{params_stub[:query]}\"")
+    end
+
+    it "doesn't show an add button" do
+      expect(rendered).not_to have_css('.memory.add')
+    end
+
+    context 'a memory' do
+      let(:memory) { memories.first }
+
+      it 'is a link to the show page for that memory' do
+        expect(rendered).to have_css("a.memory[href=\"#{memory_path(memory)}\"]")
+      end
+    end
+
+    it_behaves_like 'a memory index'
+    it_behaves_like 'paginated content'
+  end
 end
 
