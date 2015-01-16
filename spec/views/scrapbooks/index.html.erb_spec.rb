a1f117de9d854f99f4610489ca54847331e031ff
Alan Gardner <alan@armoin.com>
2015-01-16 15:46:22 +0000

    Users cannot see the creat scrapbook button unless they are logged in
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/views/scrapbooks/index.html.erb_spec.rb
+++ spec/views/scrapbooks/index.html.erb_spec.rb
@@ -1,14 +1,40 @@
 require 'rails_helper'
 
 describe 'scrapbooks/index.html.erb' do
+  let(:logged_in)        { false }
   let(:scrapbooks)       { Array.new(3) { Fabricate.build(:scrapbook) } }
   let(:paged_scrapbooks) { Kaminari.paginate_array(scrapbooks).page(1) }
   let(:memory)           { Fabricate.build(:photo_memory) }
 
-  it 'has a link to show all memories' do
-    assign(:scrapbooks, paged_scrapbooks)
-    render
-    expect(rendered).to have_link('Memories', href: memories_path)
+  before :each do
+    allow(view).to receive(:logged_in?).and_return(logged_in)
+  end
+
+  describe 'action bar' do
+    before :each do
+      assign(:scrapbooks, paged_scrapbooks)
+      render
+    end
+
+    it "has a link to show all current user's memories" do
+      expect(rendered).to have_link('Memories', href: memories_path)
+    end
+
+    context 'when the user is not logged in' do
+      let(:logged_in) { false }
+
+      it 'it does not show the "Create a scrapbook" button' do
+        expect(rendered).not_to have_link('Create a scrapbook')
+      end
+    end
+
+    context 'when the user is logged in' do
+      let(:logged_in) { true }
+
+      it 'it shows the "Create a scrapbook" button' do
+        expect(rendered).to have_link('Create a scrapbook')
+      end
+    end
   end
 
   it_behaves_like 'a scrapbook index'
--- spec/views/search/scrapbooks/index.html.erb_spec.rb
+++ spec/views/search/scrapbooks/index.html.erb_spec.rb
@@ -1,15 +1,47 @@
 require 'rails_helper'
 
 describe 'search/scrapbooks/index.html.erb' do
-  let(:user)            { Fabricate(:user) }
-  let(:query)           { 'test search'  }
-  let(:page)            { '1' }
-  let(:memory_count)    { 1 }
-  let(:results)         { double('results', paged_results: paged_scrapbooks, query: query, memory_count: memory_count, scrapbook_count: scrapbook_count) }
+  let(:user)         { Fabricate(:user) }
+  let(:logged_in)    { false }
+  let(:query)        { 'test search'  }
+  let(:page)         { '1' }
+  let(:memory_count) { 1 }
+  let(:results)      { double('results', paged_results: paged_scrapbooks, query: query, memory_count: memory_count, scrapbook_count: scrapbook_count) }
 
   before :each do
+    allow(view).to receive(:logged_in?).and_return(logged_in)
     allow(view).to receive(:current_user).and_return(user)
     assign(:results, results)
+  end
+
+  describe 'action bar' do
+    let(:scrapbook_count)  { 0 }
+    let(:paged_scrapbooks) { Kaminari.paginate_array([]).page(page) }
+
+    before :each do
+      assign(:scrapbooks, paged_scrapbooks)
+      render
+    end
+
+    it "doesn't show an add button" do
+      expect(rendered).not_to have_css('.scrapbook.add')
+    end
+
+    context 'when the user is not logged in' do
+      let(:logged_in) { false }
+
+      it 'it does not show the "Create a scrapbook" button' do
+        expect(rendered).not_to have_link('Create a scrapbook')
+      end
+    end
+
+    context 'when the user is logged in' do
+      let(:logged_in) { true }
+
+      it 'it does not show the "Create a scrapbook" button' do
+        expect(rendered).not_to have_link('Create a scrapbook')
+      end
+    end
   end
 
   context 'when there are no results' do
@@ -26,10 +58,6 @@ describe 'search/scrapbooks/index.html.erb' do
 
     it 'has a link to the memory results with the number found' do
       expect(rendered).to have_css('a.button.memories', text: "#{memory_count} memory")
-    end
-
-    it "doesn't show an add button" do
-      expect(rendered).not_to have_css('.scrapbook.add')
     end
 
     it "does not display any results" do
@@ -60,10 +88,6 @@ describe 'search/scrapbooks/index.html.erb' do
 
       it 'has a link to the memory results with the number found' do
         expect(rendered).to have_css('a.button.memories', text: "#{memory_count} memory")
-      end
-
-      it "doesn't show an add button" do
-        expect(rendered).not_to have_css('.scrapbook.add')
       end
 
       context 'a scrapbook' do
