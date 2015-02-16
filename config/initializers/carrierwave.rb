00df3747560321c5b14afb031d6a26bf3e4393e5
Alan Gardner <alan@armoin.com>
2015-02-16 13:40:12 +0000

    More housekeeping
    Signed-off-by: Alan Gardner <alan@armoin.com>

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,34 @@
+CarrierWave.configure do |config|
+  if Rails.env.test?
+    config.enable_processing = false
+  else
+    config.storage    = :fog
+    config.fog_public = true
+
+    if ENV['PROVIDER'] == 'Rackspace'
+      config.fog_credentials = {
+        :provider           => 'Rackspace',
+
+        :rackspace_username => ENV['RACKSPACE_USERNAME'],
+        :rackspace_api_key  => ENV['RACKSPACE_API_KEY'],
+        :rackspace_auth_url  => Fog::Rackspace::UK_AUTH_ENDPOINT,
+        :rackspace_region   => :lon
+      }
+    elsif ENV['PROVIDER'] == 'AWS'
+      config.fog_credentials = {
+        :provider           => 'AWS',
+
+        :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
+        :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+        :region                 => 'eu-west-1',
+      }
+    end
+
+    config.fog_directory = ENV['BUCKET_NAME']
+    config.asset_host    = ENV['ASSET_HOST']
+
+    # To let CarrierWave work on heroku
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+  end
+end
+
