cc3cf0278dbc24935803e1a3e831c483fe1ab916
Alan Gardner <alan@armoin.com>
2015-03-05 09:50:05 +0000

    Changing to use Mailgun for mail on production/staging
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- config/application.rb
+++ config/application.rb
@@ -30,13 +30,12 @@ module EdinburghStories
     config.action_mailer.default_url_options = {:host => ENV['HOST']}
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      :address        => 'smtp.sendgrid.net',
-      :port           => '587',
-      :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'heroku.com',
-      :enable_starttls_auto => true
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => ENV['MAILGUN_SMTP_DOMAIN'],
+      :authentication => :plain
     }
   end
 end
