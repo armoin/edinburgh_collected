2b2194611f712669d6d8cbd5efac0d3785a46771
Alan Gardner <alan@armoin.com>
2015-04-17 10:17:32 +0100

    Error if invalid email given to reset password
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,8 +4,13 @@ class PasswordResetsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:email])
-    @user.deliver_reset_password_instructions! if @user
-    redirect_to root_path, notice: 'Instructions have been sent to your email.'
+    if @user
+      @user.deliver_reset_password_instructions!
+      redirect_to root_path, notice: 'Instructions have been sent to your email.'
+    else
+      flash[:alert] = "Sorry but we don't recognise the email address \"#{params[:email]}\"."
+      render :new
+    end
   end
 
   def edit
