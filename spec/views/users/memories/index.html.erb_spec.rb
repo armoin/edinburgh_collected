c6d9fcc532c949d974c66794dead833a9d613bbe
Alan Gardner <urfolomeus@gmail.com>
2015-07-30 22:39:15 +0100

    Adding specs for the new users/memories index
    Signed-off-by: Alan Gardner <urfolomeus@gmail.com>

--- spec/views/users/memories/index.html.erb_spec.rb
+++ spec/views/users/memories/index.html.erb_spec.rb
@@ -1,39 +1,78 @@
 require 'rails_helper'
 
-describe 'memories/user_index.html.erb' do
-  let(:requested_user)   { Fabricate.build(:active_user, id: 123) }
-  let(:current_user)     { nil }
-  let(:memories_count)   { 0 }
-  let(:scrapbooks_count) { 0 }
+describe 'users/memories/index.html.erb' do
+  let(:requested_user) { Fabricate.build(:active_user, id: 456) }
+  let(:user)           { Fabricate.build(:active_user, id: 123) }
+
   let(:memories)         { Array.new(memories_count) {|i| Fabricate.build(:memory, id: i+1)} }
   let(:paged_memories)   { Kaminari.paginate_array(memories).page(1) }
+  let(:memories_count)   { 0 }
 
-  let(:presenter) { OpenStruct.new(
-    requested_user:    requested_user,
-    memories_count:    memories_count,
-    scrapbooks_count:  scrapbooks_count,
-    paged_memories:    paged_memories,
-    page_title:        'Foo',
-    can_add_memories?: current_user == requested_user
-  )}
+  let(:scrapbooks_count) { 0 }
+
+  let(:links) { [] }
 
   before :each do
-    allow(view).to receive(:current_user).and_return(current_user)
+    allow(view).to receive(:current_user).and_return(user)
 
-    assign(:presenter, presenter)
+    requested_user.links << links
+
+    assign(:requested_user, requested_user)
+    assign(:memories, paged_memories)
+    assign(:scrapbooks_count, scrapbooks_count)
   end
 
-  describe 'the profile header' do
-    let(:user)         { nil }
-    let(:current_user) { user }
+  it_behaves_like 'a reportable page'
 
-    it_behaves_like 'a profile headed page'
+  describe 'the profile header' do
+    before { render }
+
+    it 'displays the profile for the requested user' do
+      expect(rendered).to have_css('#profileHeader')
+    end
+
+    it "does not contain a link to the user's profile" do
+      expect(rendered).not_to have_link('View your profile', href: my_profile_path)
+    end
+
+    it "displays the requested user's avatar" do
+      expect(rendered).to have_css('img.avatar')
+    end
+
+    it "displays the requested user's username" do
+      expect(rendered).to have_css('h1.title', text: requested_user.screen_name)
+    end
+
+    it "does not display a link to edit the user's profile" do
+      expect(rendered).not_to have_link('Edit', href: my_profile_edit_path)
+    end
+
+    it "displays the requested user's description" do
+      expect(rendered).to have_css('.sub', text: requested_user.description)
+    end
+
+    context "when the user has no links" do
+      let(:links) { [] }
+
+      it "does not display the requested user's links" do
+        expect(rendered).not_to have_css('p.link')
+        expect(rendered).not_to have_css('p.link a')
+      end
+    end
+
+    context "when the user has links" do
+      let(:links) { build_array(2, :link) }
+
+      it "displays the requested user's links" do
+        links.each do |link|
+          expect(rendered).to have_css("p.link a[href=\"#{link.url}\"]", text: link.name, count: 1)
+        end
+      end
+    end
   end
 
   describe 'the action bar' do
-    before :each do
-      render
-    end
+    before { render }
 
     describe 'the total memories count' do
       context 'when there are 0 memories' do
@@ -62,11 +101,7 @@ describe 'memories/user_index.html.erb' do
     end
 
     describe 'the scrapbooks link' do
-      before :each do
-        render
-      end
-
-      it 'links to the scrapbooks for the requested user' do
+      it 'links to the scrapbooks for the current user' do
         expect(rendered).to have_link('scrapbooks', href: user_scrapbooks_path(user_id: requested_user.id))
       end
 
@@ -97,29 +132,13 @@ describe 'memories/user_index.html.erb' do
       end
     end
 
-    describe 'the Add a memory button' do
-      context 'when the requested user is the current user' do
-        let(:current_user) { requested_user }
-
-        it 'shows the Add a memory button' do
-          expect(rendered).to have_link('Add a memory', href: new_my_memory_path)
-        end
-      end
-
-      context 'when the requested user is not the current user' do
-        let(:current_user) { Fabricate.build(:active_user, id: 456) }
-
-        it 'does not show the Add a memory button' do
-          expect(rendered).not_to have_link('Add a memory', href: new_my_memory_path)
-        end
-      end
+    it 'does not show the Add a memory button' do
+      expect(rendered).to_not have_link('Add a memory', href: new_my_memory_path)
     end
   end
 
   describe 'the memory index' do
-    before :each do
-      render
-    end
+    before { render }
 
     context 'when there are no memories' do
       let(:memories_count)   { 0 }
