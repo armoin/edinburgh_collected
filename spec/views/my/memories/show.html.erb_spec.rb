bfeec7b65c591355a6f52fe65561d3d98c607aee
Alan Gardner <urfolomeus@gmail.com>
2015-08-02 12:22:39 +0100

    Moving back button behaviour to individual show pages - my memories

--- spec/views/my/memories/show.html.erb_spec.rb
+++ spec/views/my/memories/show.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 describe "my/memories/show.html.erb" do
+  describe "action bar" do
+    let(:owner)  { Fabricate.build(:active_user, id: 123) }
+    let(:memory) { Fabricate.build(:photo_memory, id: 456, user: owner) }
+
+    before :each do
+      assign(:memory, memory)
+      render
+    end
+
+    it "has a back button to the my memories index page" do
+      expect(rendered).to have_link('Back', href: my_memories_path)
+    end
+  end
+
   it_behaves_like 'a memory page'
 end
