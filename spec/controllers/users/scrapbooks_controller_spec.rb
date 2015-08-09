b2914db04cf17101aecf41cda9ee26a2ba595d10
Alan Gardner <urfolomeus@gmail.com>
2015-08-09 23:14:24 +0100

    Refactoring the users/scrapbooks_controller specs
    Signed-off-by: Alan Gardner <urfolomeus@gmail.com>

--- spec/controllers/users/scrapbooks_controller_spec.rb
+++ spec/controllers/users/scrapbooks_controller_spec.rb
@@ -2,29 +2,49 @@ require 'rails_helper'
 
 describe Users::ScrapbooksController do
   let(:requested_user)          { Fabricate.build(:active_user, id: 123) }
+
+  let(:user_find_result)        { requested_user }
+  let(:current_user)            { nil }
+  let(:visible)                 { true }
+
   let(:scrapbooks_stub)         { double('scrapbooks') }
   let(:visible_scrapbooks_stub) { double('visible_scrapbooks') }
 
   before :each do
-    allow(User).to receive(:find).and_return(requested_user)
+    allow(User).to receive(:find) { user_find_result }
+
+    allow(controller).to receive(:current_user).and_return(current_user)
+
+    allow(requested_user).to receive(:publicly_visible?).and_return(visible)
+
+    allow(requested_user).to receive(:scrapbooks).and_return(scrapbooks_stub)
+    allow(scrapbooks_stub).to receive(:publicly_visible).and_return(visible_scrapbooks_stub)
   end
 
   describe 'GET index' do
-    it 'sets the current scrapbook index path' do
+    let(:ordered_scrapbooks_stub) { double('ordered_scrapbooks') }
+    let(:stub_presenter)          { double('presenter') }
+    let(:stub_memory_fetcher)     { double('memory_fetcher') }
+
+    before :each do
+      allow(visible_scrapbooks_stub).to receive(:by_last_created).and_return(ordered_scrapbooks_stub)
+
+      allow(ApprovedScrapbookMemoryFetcher).to receive(:new).with(ordered_scrapbooks_stub).and_return(stub_memory_fetcher)
+      allow(ScrapbookIndexPresenter).to receive(:new).and_return(stub_presenter)
+
       get :index, user_id: requested_user.to_param
+    end
+
+    it 'sets the current scrapbook index path' do
       expect(session[:current_scrapbook_index_path]).to eql(user_scrapbooks_path(user_id: requested_user.to_param))
     end
 
     it 'finds the requested user' do
-      get :index, user_id: requested_user.to_param
       expect(User).to have_received(:find).with(requested_user.to_param)
     end
 
     context 'when no user is found' do
-      before :each do
-        allow(User).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
-        get :index, user_id: requested_user.to_param
-      end
+      let(:user_find_result) { fail ActiveRecord::RecordNotFound }
 
       it 'returns a 404 error' do
         expect(response).to be_not_found
@@ -32,39 +52,22 @@ describe Users::ScrapbooksController do
     end
 
     context 'when requested user is found' do
+      let(:user_find_result) { requested_user }
+
       it 'assigns the requested user' do
-        get :index, user_id: requested_user.to_param
         expect(assigns[:requested_user]).to eql(requested_user)
       end
 
       context 'if the requested user is the current user' do
+        let(:current_user) { requested_user }
+
         it 'redirects to my/scrapbooks' do
-          allow(controller).to receive(:current_user).and_return(requested_user)
-          get :index, user_id: requested_user.to_param
           expect(response).to redirect_to(my_scrapbooks_path)
         end
       end
 
       context 'if the requested user is not the current user' do
-        let(:other_user)              { Fabricate.build(:active_user, id: 456) }
-        let(:ordered_scrapbooks_stub) { double('ordered_scrapbooks') }
-        let(:stub_presenter)          { double('presenter') }
-        let(:stub_memory_fetcher)     { double('memory_fetcher') }
-
-        before :each do
-          allow(controller).to receive(:current_user).and_return(other_user)
-
-          allow(requested_user).to receive(:scrapbooks).and_return(scrapbooks_stub)
-          allow(requested_user).to receive(:publicly_visible?).and_return(visible)
-
-          allow(scrapbooks_stub).to receive(:publicly_visible).and_return(visible_scrapbooks_stub)
-          allow(visible_scrapbooks_stub).to receive(:by_last_created).and_return(ordered_scrapbooks_stub)
-
-          allow(ApprovedScrapbookMemoryFetcher).to receive(:new).with(ordered_scrapbooks_stub).and_return(stub_memory_fetcher)
-          allow(ScrapbookIndexPresenter).to receive(:new).and_return(stub_presenter)
-
-          get :index, user_id: requested_user.to_param
-        end
+        let(:current_user) { nil }
 
         context 'but the requested user is not approved' do
           let(:visible) { false }
@@ -105,20 +108,10 @@ describe Users::ScrapbooksController do
   describe 'GET show' do
     let(:scrapbook)          { Fabricate.build(:scrapbook, id: 456, user: requested_user) }
     let(:format)             { 'html' }
-    let(:user_find_result)   { requested_user }
-    let(:current_user)       { nil }
-    let(:visible)            { true }
     let(:memories)           { double('scrapbook memories') }
     let(:paginated_memories) { double('paginated memories') }
 
     before :each do
-      allow(User).to receive(:find) { user_find_result }
-      allow(controller).to receive(:current_user).and_return(current_user)
-
-      allow(requested_user).to receive(:publicly_visible?).and_return(visible)
-
-      allow(requested_user).to receive(:scrapbooks).and_return(scrapbooks_stub)
-      allow(scrapbooks_stub).to receive(:publicly_visible).and_return(visible_scrapbooks_stub)
       allow(visible_scrapbooks_stub).to receive(:find).and_return(scrapbook)
 
       allow(scrapbook).to receive(:ordered_memories).and_return(memories)
