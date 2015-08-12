9f884c29d45741210368ecd405d514b6a7297c5c
Alan Gardner <urfolomeus@gmail.com>
2015-08-12 16:40:42 +0100

    Change back button to list button for moderating memories
    Signed-off-by: Alan Gardner <urfolomeus@gmail.com>

--- spec/views/admin/moderation/memories/show.html.erb_spec.rb
+++ spec/views/admin/moderation/memories/show.html.erb_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe 'admin/moderation/memories/show.html.erb' do
-  it_behaves_like 'a memory page'
-
   describe 'moderation actions' do
     let(:user)         { Fabricate.build(:admin_user, id: 456) }
     let(:reason)       { nil }
@@ -18,4 +16,20 @@ describe 'admin/moderation/memories/show.html.erb' do
 
     it_behaves_like 'a moderated show page'
   end
+
+  describe "action bar" do
+    let(:user)   { Fabricate.build(:active_user, id: 123) }
+    let(:memory) { Fabricate.build(:photo_memory, id: 456, user: user) }
+
+    before :each do
+      assign(:memory, memory)
+      render
+    end
+
+    it 'has a back button to "More memories from this user"' do
+      expect(rendered).to have_link('More memories from this user', href: user_memories_path(user))
+    end
+  end
+
+  it_behaves_like 'a memory page'
 end
