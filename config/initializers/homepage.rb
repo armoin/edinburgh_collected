0c31dd1f0020453808746d72c6a618a1d110201f
Alan Gardner <urfolomeus@gmail.com>
2015-09-09 11:43:20 +0100

    Updating homepage after review
    Signed-off-by: Alan Gardner <urfolomeus@gmail.com>

--- config/initializers/homepage.rb
+++ config/initializers/homepage.rb
@@ -1,44 +1,44 @@
 BASE_CDN_URL = 'https://4019eac0904bfa4d1cb5-9a025cf9f86362160bae9a5684264911.ssl.cf3.rackcdn.com'
 
 HOMEPAGE_DATA = OpenStruct.new(
-  hero_image: 'photos/majorettes.jpg',
+  hero_image: 'photos/gala_day.jpg',
   featured_memory_data: {
-    id: 606,
-    title: 'Twirlers - More Than A Newspaper',
-    year: 1985,
-    user: {
-      id: 7,
-      name: 'From There To Here'
-    }
-  },
-  featured_scrapbook_data: {
-    id: 79,
-    title: 'Kathleen Glancy\'s Dean School Days',
+    id: 488,
+    title: 'Kathleen Glancy\'s Highland Dancing Class at a gala day',
+    year: 1955,
     user: {
       id: 132,
       name: 'Dean Village Memories'
+    }
+  },
+  featured_scrapbook_data: {
+    id: 87,
+    title: 'Venchie Playgrounds',
+    user: {
+      id: 7,
+      name: 'From There To Here'
     },
-    memory_ids: [444, 453, 445, 455],
+    memory_ids: [503, 500, 505, 504],
     memories: [
       {
-        id: 444,
-        image_url: '74505d11-d8ff-4145-8eaf-74a15ef0c698.jpg',
-        title: 'Kathleen Glancy\'s class photograph from P1'
+        id: 503,
+        image_url: 'ef0456a2-5474-4666-897a-a7e29df6e206.jpg',
+        title: 'Carnival at the Venchie'
       },
       {
-        id: 453,
-        image_url: '47ae4f50-bdb4-4645-8eb3-ea101853171e.jpg',
-        title: 'Dean School badge from Kathleen Glancy\'s school uniform'
+        id: 500,
+        image_url: 'ce86c163-1db5-4848-af7f-28974f932663.jpg',
+        title: 'Venchie at night'
       },
       {
-        id: 445,
-        image_url: '4decb815-83a0-4124-9419-53834965eb41.jpg',
-        title: 'Kathleen Glancy with school friend'
+        id: 505,
+        image_url: '4cebe7fd-5c73-4892-8ceb-15c974eda19a.jpg',
+        title: 'Clovenstone Venchie'
       },
       {
-        id: 455,
-        image_url: '2b06310f-a3e1-425f-9c3d-c2e07c588435.jpg',
-        title: 'Burns Federation Certificate awarded to Kathleen Glancy'
+        id: 504,
+        image_url: 'b12d6643-585c-468a-a95d-0db0c92a5f92.jpg',
+        title: 'Venchie Fun'
       },
     ]
   }
