f36e32713de3d3019a5611f4ae735e73b042eab9
Alan Gardner <urfolomeus@gmail.com>
2015-10-18 11:11:06 +0100

    Trying to get rid of random user uniqueness errors

--- spec/fabricators/scrapbook_fabricator.rb
+++ spec/fabricators/scrapbook_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:scrapbook) do
   id               { sequence(:id, 1) }
-  user
+  user             { Fabricate(:active_user) }
   title            { Faker::Product.product_name }
   description      { Faker::Lorem.words(rand(50)).join(' ') }
   updated_at       { rand(10).to_i.days.ago }
