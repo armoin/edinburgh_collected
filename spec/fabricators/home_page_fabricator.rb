6ceeb9eaa818f146233c8aee4be932165dfb7259
Alan Gardner <urfolomeus@gmail.com>
2016-03-08 17:11:57 +0000

    Tidying up home page fabricator

--- spec/fabricators/home_page_fabricator.rb
+++ spec/fabricators/home_page_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:home_page) do
-  featured_memory               { featured_memory }
+  featured_memory               { Fabricate(:approved_photo_memory) }
   featured_scrapbook            { featured_scrapbook }
   featured_scrapbook_memory_ids { featured_scrapbook.memories.map(&:id).join(',') }
 end
@@ -12,13 +12,8 @@ Fabricator(:published_home_page, from: :home_page) do
   published true
 end
 
-def featured_memory
-  Fabricate(:approved_memory)
-end
-
 def featured_scrapbook
   scrapbook = Fabricate(:approved_scrapbook)
-  memories = Fabricate.times(4, :approved_memory)
-  scrapbook.memories << memories
+  scrapbook.memories << Fabricate.times(4, :approved_photo_memory)
   scrapbook
 end
